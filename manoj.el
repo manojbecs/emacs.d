a6be05dfe159adce94ab102bb479edbdb45977a9
Manoj <manoj@manoj.(none)>
2011-08-08 20:44:40 +0530

    Laptop setip

--- manoj.el
+++ manoj.el
@@ -6,12 +6,6 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/rinari")
 (require 'rinari)
 
-;;; rhtml-mode
-(add-to-list 'load-path "~/.emacs.d/vendor/rhtml")
-(require 'rhtml-mode)
-(add-hook 'rhtml-mode-hook
-     	  (lambda () (rinari-launch)))
-
 ;; Rinari tage
 (setq rinari-tags-file-name "TAGS")
 
@@ -31,11 +25,6 @@
 (require 'project-mode)
 (project-load-all)
 
-;;; TwiterMode - http://www.emacswiki.org/emacs/TwitteringMode
-(add-to-list 'load-path "~/.emacs.d/vendor/twittering-mode")
-(require 'twittering-mode)
-;;; (twittering-icon-mode) ;;; Shows profile icons
-
 ;; Gnus for GMail
 ;; (add-to-list 'gnus-secondary-select-methods '(nnimap "gmail"
 ;;                                   (nnimap-address "imap.gmail.com")
@@ -48,12 +37,6 @@
 
 ;; When loading files reuse existing frames.
 (setq gnuserv-frame (car (frame-list)))
-
-;; android-mode
-(add-to-list 'load-path "~/.emacs.d/vendor/android-mode")
-(require 'android-mode)
-;;; (defcustom android-mode-sdk-dir "~/opt/android");; Find out why
-;;; this is failing
 
 (global-set-key (kbd "C-x m") 'eshell-command)
 
@@ -96,14 +79,6 @@
 (global-set-key [f11] 'fullscreen)
 (global-set-key [f9] 'dictionary-search)
 
-(add-to-list 'load-path "~/.emacs.d/vendor/cucumber.el")
-(require 'feature-mode)
-(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
-(add-to-list 'auto-mode-alist '("\\.js.erb$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.xml.builder$" . ruby-mode))
-(add-to-list 'load-path "~/.emacs.d/vendor/rspec-mode")
-(require 'rspec-mode)
-
 (global-set-key [f6] 'rspec-verify-single)
 
 (require 'rvm)
@@ -116,12 +91,6 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/php-mode")
 (require 'php-mode)
 
-;; coffee-mode
-(add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
-(require 'coffee-mode)
-
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
 
 ;;Flymake PHP Extension
 (require 'flymake) (unless (fboundp 'flymake-php-init) (defun flymake-php-init () (let* ((temp-file (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace)) (local-file (file-relative-name temp-file (file-name-directory buffer-file-name)))) (list "php" (list "-f" local-file "-l"))))) (let ((php-ext-re "\.php[345]?\'") (php-error-re "\(?:Parse\|Fatal\) error: \(.\) in \(.\) on line \([0-9]+\)")) (unless (assoc php-ext-re flymake-allowed-file-name-masks) (add-to-list 'flymake-allowed-file-name-masks (list php-ext-re 'flymake-php-init 'flymake-simple-cleanup 'flymake-get-real-file-name)) (add-to-list 'compilation-error-regexp-alist-alist (list 'compilation-php php-error-re 2 3 nil nil)) (add-to-list 'compilation-error-regexp-alist 'compilation-php) (add-to-list 'flymake-err-line-patterns (list php-error-re 2 3 nil 1)))) ;; add php flymake support (add-hook 'php-mode-hook (lambda () (flymake-mode t)))
